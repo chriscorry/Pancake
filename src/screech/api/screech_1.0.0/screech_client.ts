eb54bb9b9773ed3901cec1ca1fb94d7979e09f4d
Chris Corry <chris@thecorrys.com>
2018-02-08 14:42:32 -0800

    Initial version of the ClientAPI base class, with the Screech API updated to use it. Also some NASTY connection-related bug fixes made inside the ClientAPI class.

--- src/screech/api/screech_1.0.0/screech_client.ts
+++ src/screech/api/screech_1.0.0/screech_client.ts
@@ -4,9 +4,11 @@
  **                                                                        **
  ****************************************************************************/
 
-import EventEmitter      = require('events');
-import socketIOClient    = require('socket.io-client');
+// import EventEmitter      = require('events');
+// import socketIOClient    = require('socket.io-client');
 
+import { ListenerCallback,
+         ClientAPI }     from '../../../util/clientapi';
 import * as utils        from '../../../util/pancake-utils';
 import { PancakeError }  from '../../../util/pancake-err';
 import { Configuration } from '../../../util/pancake-config';
@@ -19,20 +21,6 @@ const log = utils.log;
  **                                                                        **
  ****************************************************************************/
 
-// URLs
-const URL_HTTP                   = 'http://';
-const URL_HTTPS                  = 'https://';
-const SCREECH_RECONNECT_INTERVAL = 15; // sec
-const SCREECH_CONNECT_TIMEOUT    = 10; // sec
-
-// Other constants
-const HTTP_REQUEST_HEADER = {
-  headers: { 'Content-Type': 'application/json', 'Accept-Version': "1" }
-}
-
-type ListenerCallback = (...args: any[]) => void;
-type DisconnectCallback = (socket:any) => void;
-
 
 /****************************************************************************
  **                                                                        **
@@ -40,15 +28,8 @@ type DisconnectCallback = (socket:any) => void;
  **                                                                        **
  ****************************************************************************/
 
-export class ScreechClient extends EventEmitter
+export class ScreechClient extends ClientAPI
 {
-  private _screechBaseURL: string;
-  private _connected = false;
-  private _reconnecting = false;
-  private _reconnectInterval = SCREECH_RECONNECT_INTERVAL;
-  private _timerID: NodeJS.Timer;
-  private _lastError: any;
-  private _screechSocket: any;
   private _activeDomains = new Set<[string, string, any]>();
   private _activeChannels = new Set<[string, string, string, any]>();
   private _activeSubs = new Set<[string,string]>();
@@ -58,180 +39,51 @@ export class ScreechClient extends EventEmitter
 
   /****************************************************************************
    **                                                                        **
+   ** Overrides                                                              **
+   **                                                                        **
+   ****************************************************************************/
+
+  protected async _performReconnectTasks() : Promise<void>
+  {
+    let waitArray: Promise<PancakeError>[] = [];
+
+    // Re-setup domains first, then channels, then subs
+    this._activeDomains.forEach((domain) => {
+      waitArray.push(this.createDomain.apply(this, domain));
+    });
+    await Promise.all(waitArray);
+    waitArray = [];
+    this._activeChannels.forEach((channel) => {
+      waitArray.push(this.openChannel.apply(this, channel));
+    });
+    await Promise.all(waitArray);
+    waitArray = [];
+    this._activeSubs.forEach((sub) => {
+      waitArray.push(this._subscribe.apply(this, sub));
+    });
+    await Promise.all(waitArray);
+  }
+
+
+  protected _performConnectCleanup() : void
+  {
+    // Clear out active domains, channels, and subscriptions
+    this._activeDomains.clear();
+    this._activeChannels.clear();
+    this._activeSubs.clear();
+  }
+
+
+  /****************************************************************************
+   **                                                                        **
    ** Private methods                                                        **
    **                                                                        **
    ****************************************************************************/
 
-  private _processError(status: string, reason?: string, obj?: any, logError: boolean = true) : PancakeError
-  {
-    this._lastError = new PancakeError(status, reason, obj);
-    if (true === logError) {
-      log.trace(`SCREECH: ${status}: ${reason}`);
-      if (obj) log.trace(obj);
-    }
-    return this._lastError;
-  }
-
-
-  private _timeoutCallback(callback: Function) : () => void
-  {
-    let called = false;
-
-    let timerID = setTimeout(() => {
-      if (called) return;
-        called = true;
-        callback(new PancakeError('ERR_CALLBACK_TIMEOUT'));
-    },
-    SCREECH_CONNECT_TIMEOUT*1000);
-
-    return function() {
-      if (called) return;
-      called = true;
-      clearTimeout(timerID);
-      callback.apply(this, arguments);
-    }
-  }
-
-
-  private _onDisconnect(socket: any) : void
-  {
-    // Let everyone know
-    this.emit('disconnect', socket);
-    log.info(`SCREECH: Lost connection to Screech server.`);
-
-    // Try again
-    this._initiateReconnects();
-  }
-
-
-  private _reconnect() : void
-  {
-    log.info('SCREECH: Trying to establish connection with Screech server.');
-
-    // Give it a shot
-    this._connect(false);
-  }
-
-
-  private _cancelReconnects() : void
-  {
-    if (this._timerID) {
-      clearTimeout(this._timerID);
-      this._timerID = undefined;
-    }
-    this._reconnecting = false;
-  }
-
-
-  private _initiateReconnects() : void
-  {
-    this._cancelReconnects();
-    this._screechSocket = undefined;
-    this._connected = false;
-    this._reconnecting = true;
-    this._timerID = setTimeout(() => { this._reconnect(); }, this._reconnectInterval*1000);
-  }
-
-
-  private async _postRegistration(socket: any) : Promise<void>
-  {
-    // Remember save off vitals
-    this._screechSocket = socket;
-    this._connected = true;
-    let resubscribe = this._reconnecting;
-
-    // Cancel any reconnect attempts
-    this._cancelReconnects();
-
-    // Make sure we receive important notifications
-    this._screechSocket.on('disconnect', () => { this._onDisconnect(socket); });
-
-    // Let everyone know
-    this.emit('connect');
-
-    // Hook everything back up again
-    if (true === resubscribe) {
-
-      let waitArray: Promise<PancakeError>[] = [];
-
-      // Re-setup domains first, then channels, then subs
-      this._activeDomains.forEach((domain) => {
-        waitArray.push(this.createDomain.apply(this, domain));
-      });
-      await Promise.all(waitArray);
-      waitArray = [];
-      this._activeChannels.forEach((channel) => {
-        waitArray.push(this.openChannel.apply(this, channel));
-      });
-      await Promise.all(waitArray);
-      waitArray = [];
-      this._activeSubs.forEach((sub) => {
-        waitArray.push(this._subscribe.apply(this, sub));
-      });
-      await Promise.all(waitArray);
-    }
-  }
-
-
-  private async _connect(logErrors: boolean) : Promise<PancakeError>
-  {
-    let socketClient: any;
-
-    return new Promise<PancakeError>((resolve, reject) => {
-
-      // Kick it all off
-      try {
-
-        // Make our websocket connect
-        socketClient = socketIOClient(this._screechBaseURL + '/', { reconnection: false });
-        if (!socketClient) {
-          this._initiateReconnects();
-          this._processError('ERR_SCREECH_CONNECT', `SCREECH: Could not connect to Screech server ${this._screechBaseURL}`, undefined, logErrors);
-        }
-
-        // Get access to the Screech API
-        socketClient.emit('negotiate', { name: 'screech', ver: '1.0.0' }, this._timeoutCallback((negotiateResp: any) => {
-
-          // Timeout?
-          if (!(negotiateResp instanceof PancakeError)) {
-
-            // Everything okay?
-            if (negotiateResp[0].status === 'SUCCESS') {
-
-              // We are connected
-              this._postRegistration(socketClient);
-              resolve();
-            }
-            else if (true === logErrors) {
-              reject(this._processError('ERR_SCREECH_NEGOTIATE', `SCREECH: Could not negotiate Screech API with server`, negotiateResp[0]));
-            }
-          }
-
-          // Callback timeout
-          else {
-            socketClient.close();
-            socketClient = undefined;
-            this._initiateReconnects();
-            this._processError('ERR_SCREECH_NEGOTIATE_TIMEOUT', 'Screech API negotiate timeout.', undefined, logErrors);
-          }
-        }));
-
-      } catch (error) {
-        socketClient.close();
-        socketClient = undefined;
-        this._initiateReconnects();
-        return this._processError('ERR_SCREECH_CONNECT', `SCREECH: Could not connect to Screech server ${this._screechBaseURL}`, undefined, logErrors);
-      }
-
-      return;
-    });
-  }
-
-
   async _subscribe(domainName: string, channelName: string, onMessage?: ListenerCallback) : Promise<PancakeError>
   {
     // Simple validation checks
-    if (!this._connected) {
+    if (!this.connected) {
       return this._processError('ERR_NO_CONNECTION', `SCREECH: Not connected to server.`);
     }
 
@@ -247,7 +99,7 @@ export class ScreechClient extends EventEmitter
 
     // Kick off the request
     return new Promise<PancakeError>((resolve, reject) => {
-      this._screechSocket.emit('screech:subscribe', { domain: domainName, channel: channelName }, this._timeoutCallback((resp: any) => {
+      this._socket.emit('screech:subscribe', { domain: domainName, channel: channelName }, this._timeoutCallback((resp: any) => {
 
         if (!(resp instanceof PancakeError)) {
 
@@ -256,8 +108,8 @@ export class ScreechClient extends EventEmitter
 
             // Add event listeners
             // We ALWAYS need this step
-            this._screechSocket.on(domainName + '-' + channelName, (message:any) => {
-              this.emit(channelName, message);
+            this._socket.on(domainName + '-' + channelName, (message:any) => {
+              this.emit(domainName + '-' + channelName, message);
             });
 
             // We only do these steps the first time (i.e., not on reconnects)
@@ -286,46 +138,28 @@ export class ScreechClient extends EventEmitter
    **                                                                        **
    ****************************************************************************/
 
-  async connect(address: string, port: number, onConnect: ListenerCallback = undefined, onDisconnect: DisconnectCallback = undefined) : Promise<PancakeError>
-  {
-    // CLose up shop and cancel any reconnect attempts
-    if (this._screechSocket) {
-      this._screechSocket.close();
-      this._screechSocket = undefined;
-    }
-    this._connected = false;
-    this._cancelReconnects();
+   constructor()
+   {
+     super('Screech', 'screech', '1.0.0');
+   }
 
-    // Clear out active domains, channels, and subscriptions
-    this._activeDomains.clear();
-    this._activeChannels.clear();
-    this._activeSubs.clear();
 
-    // Clear out old event handlers
-    this.removeAllListeners();
-
-    // Remember these callbacks
-    if (onConnect) this.on('connect', onConnect);
-    if (onDisconnect) this.on('disconnect', onDisconnect);
-
-    // Build our URL
-    this._screechBaseURL = URL_HTTP + address + ':' + port;
-
-    // Kick it all off
-    return this._connect(true);
-  }
-
+  /****************************************************************************
+   **                                                                        **
+   ** Public methods                                                         **
+   **                                                                        **
+   ****************************************************************************/
 
   async createDomain(domainName: string, description: string, opts?: any) : Promise<PancakeError>
   {
     // Simple validation checks
-    if (!this._connected) {
+    if (!this.connected) {
       return this._processError('ERR_NO_CONNECTION', `SCREECH: Not connected to server.`);
     }
 
     // Kick off the request
     return new Promise<PancakeError>((resolve, reject) => {
-      this._screechSocket.emit('screech:createDomain', { name: domainName, description, opts }, this._timeoutCallback((resp: any) => {
+      this._socket.emit('screech:createDomain', { name: domainName, description, opts }, this._timeoutCallback((resp: any) => {
 
         if (!(resp instanceof PancakeError)) {
 
@@ -354,13 +188,13 @@ export class ScreechClient extends EventEmitter
   async deleteDomain(domainName: string) : Promise<PancakeError>
   {
     // Simple validation checks
-    if (!this._connected) {
+    if (!this.connected) {
       return this._processError('ERR_NO_CONNECTION', `SCREECH: Not connected to server.`);
     }
 
     // Kick off the request
     return new Promise<PancakeError>((resolve, reject) => {
-      this._screechSocket.emit('screech:deleteDomain', { name: domainName }, this._timeoutCallback((resp: any) => {
+      this._socket.emit('screech:deleteDomain', { name: domainName }, this._timeoutCallback((resp: any) => {
 
         if (!(resp instanceof PancakeError)) {
 
@@ -397,13 +231,13 @@ export class ScreechClient extends EventEmitter
   async openChannel(domainName: string, channelName: string, description: string, opts?: any) : Promise<PancakeError>
   {
     // Simple validation checks
-    if (!this._connected) {
+    if (!this.connected) {
       return this._processError('ERR_NO_CONNECTION', `SCREECH: Not connected to server.`);
     }
 
     // Kick off the request
     return new Promise<PancakeError>((resolve, reject) => {
-      this._screechSocket.emit('screech:openChannel', { domain: domainName, name: channelName, description, opts }, this._timeoutCallback((resp: any) => {
+      this._socket.emit('screech:openChannel', { domain: domainName, name: channelName, description, opts }, this._timeoutCallback((resp: any) => {
 
         if (!(resp instanceof PancakeError)) {
 
@@ -434,13 +268,13 @@ export class ScreechClient extends EventEmitter
   async deleteChannel(domainName: string, channelName: string) : Promise<PancakeError>
   {
     // Simple validation checks
-    if (!this._connected) {
+    if (!this.connected) {
       return this._processError('ERR_NO_CONNECTION', `SCREECH: Not connected to server.`);
     }
 
     // Kick off the request
     return new Promise<PancakeError>((resolve, reject) => {
-      this._screechSocket.emit('screech:deleteChannel', { domain: domainName, name: channelName }, this._timeoutCallback((resp: any) => {
+      this._socket.emit('screech:deleteChannel', { domain: domainName, name: channelName }, this._timeoutCallback((resp: any) => {
 
         if (!(resp instanceof PancakeError)) {
 
@@ -460,7 +294,7 @@ export class ScreechClient extends EventEmitter
             this._activeSubs = utils.filterSet(this._activeSubs, (value: [string, string]) => {
               if (value[0] === domainName && value[1] === channelName) {
                 this.removeAllListeners(domainName + '-' + channelName);
-                this._screechSocket.removeAllListeners(domainName + '-' + channelName);
+                this._socket.removeAllListeners(domainName + '-' + channelName);
                 return false;
               }
               return true;
@@ -483,13 +317,13 @@ export class ScreechClient extends EventEmitter
   async send(domainName: string, channelName: string, payload: any) : Promise<PancakeError>
   {
     // Simple validation checks
-    if (!this._connected) {
+    if (!this.connected) {
       return this._processError('ERR_NO_CONNECTION', `SCREECH: Not connected to server.`);
     }
 
     // Kick off the request
     return new Promise<PancakeError>((resolve, reject) => {
-      this._screechSocket.emit('screech:send', { domain: domainName, channel: channelName, payload }, this._timeoutCallback((resp: any) => {
+      this._socket.emit('screech:send', { domain: domainName, channel: channelName, payload }, this._timeoutCallback((resp: any) => {
 
         if (!(resp instanceof PancakeError)) {
 
@@ -525,18 +359,6 @@ export class ScreechClient extends EventEmitter
   async subscribe(domainName: string, channelName: string, onMessage: ListenerCallback) : Promise<PancakeError>
   {
     return this._subscribe(domainName, channelName, onMessage);
-  }
-
-
-  get lastError() : PancakeError
-  {
-    return this._lastError;
-  }
-
-
-  get connected() : boolean
-  {
-    return this._connected;
   }
 
 } // END class ScreechClient
---
+++ src/util/clientapi.ts
@@ -0,0 +1,315 @@
+/****************************************************************************
+ **                                                                        **
+ ** Modules                                                                **
+ **                                                                        **
+ ****************************************************************************/
+
+import EventEmitter      = require('events');
+import socketIOClient    = require('socket.io-client');
+
+import * as utils        from './pancake-utils';
+import { PancakeError }  from './pancake-err';
+import { Configuration } from './pancake-config';
+const log = utils.log;
+
+
+/****************************************************************************
+ **                                                                        **
+ ** Vars & definitions                                                     **
+ **                                                                        **
+ ****************************************************************************/
+
+// URLs
+const URL_HTTP           = 'http://';
+const URL_HTTPS          = 'https://';
+const RECONNECT_INTERVAL = 15; // sec
+const CONNECT_TIMEOUT    = 10; // sec
+
+// Other constants
+const HTTP_REQUEST_HEADER = {
+  headers: { 'Content-Type': 'application/json', 'Accept-Version': "1" }
+}
+
+export type ListenerCallback = (...args: any[]) => void;
+export type DisconnectCallback = (socket:any) => void;
+
+
+/****************************************************************************
+ **                                                                        **
+ ** Class ClientAPI                                                        **
+ **                                                                        **
+ ****************************************************************************/
+
+export class ClientAPI extends EventEmitter
+{
+  private _baseURL: string;
+  private _connected = false;
+  private _reconnecting = false;
+  private _reconnectInterval = RECONNECT_INTERVAL;
+  private _timerID: NodeJS.Timer;
+  private _lastError: any;
+  protected _socket: any;
+
+  // Provided by subclasses
+  private _serviceName: string;
+  private _serviceNameUCase: string;
+  private _serverAPI: string;
+  private _serverAPIVer: string;
+
+
+  /****************************************************************************
+   **                                                                        **
+   ** Private methods                                                        **
+   **                                                                        **
+   ****************************************************************************/
+
+  private _onDisconnect(socket: any) : void
+  {
+    // Let everyone know
+    this.emit('disconnect', socket);
+    log.info(`${this._serviceNameUCase}: Lost connection to ${this._serviceName} server.`);
+
+    // Try again
+    this._initiateReconnects();
+  }
+
+
+  private _cancelReconnects() : void
+  {
+    if (this._timerID) {
+      clearTimeout(this._timerID);
+      this._timerID = undefined;
+    }
+    this._reconnecting = false;
+  }
+
+
+  private _initiateReconnects(connectFunc?: Function, passThis: boolean = false) : void
+  {
+    this._cancelReconnects();
+    this._socket = undefined;
+    this._connected = false;
+    this._reconnecting = true;
+    this._timerID = setTimeout(() => {
+
+      // Let folks know
+      log.info(`${this._serviceNameUCase}: Trying to establish connection with ${this._serviceName} server.`);
+
+      // Give it another shot
+      if (!connectFunc) {
+        this._connect(false);
+      }
+      else {
+        passThis ? connectFunc(this, false) : connectFunc.bind(this, false);
+      }
+     }, this._reconnectInterval*1000);
+  }
+
+
+  private async _postRegistration(socket: any) : Promise<void>
+  {
+    // Remember save off vitals
+    this._socket = socket;
+    this._connected = true;
+    let resubscribe = this._reconnecting;
+
+    // Cancel any reconnect attempts
+    this._cancelReconnects();
+
+    // Make sure we receive important notifications
+    this._socket.on('disconnect', () => { this._onDisconnect(socket); });
+
+    // Let everyone know
+    this.emit('connect');
+
+    // Hook everything back up again
+    if (true === resubscribe) {
+      this._performReconnectTasks();
+    }
+  }
+
+
+  private async _connect(logErrors: boolean) : Promise<PancakeError>
+  {
+    let socketClient: any;
+
+    return new Promise<PancakeError>((resolve, reject) => {
+
+      function _innerConnect(client: ClientAPI, logErrorsInner: boolean) : PancakeError
+      {
+        // Make our websocket connect
+        socketClient = socketIOClient(client._baseURL + '/', { reconnection: false });
+        if (!socketClient) {
+          client._initiateReconnects(_innerConnect, true);
+          return client._processError('ERR_CLIENT_CONNECT', `${client._serviceNameUCase}: Could not connect to ${client._serviceName} server ${client._baseURL}`, undefined, logErrorsInner);
+        }
+
+        // Kick it all off
+        try {
+
+          // Get access to the API
+          socketClient.emit('negotiate', { name: client._serverAPI, ver: client._serverAPIVer }, client._timeoutCallback((negotiateResp: any) => {
+
+            // Timeout?
+            if (!(negotiateResp instanceof PancakeError)) {
+
+              // Everything okay?
+              if (negotiateResp[0].status === 'SUCCESS') {
+
+                // We are connected
+                client._postRegistration(socketClient);
+                resolve();
+              }
+              else if (true === logErrors) {
+                reject(client._processError('ERR_CLIENT_NEGOTIATE', `${client._serviceNameUCase}: Could not negotiate ${client._serviceName} API with server`, negotiateResp[0]));
+              }
+            }
+
+            // Callback timeout
+            else {
+              socketClient.close();
+              socketClient = undefined;
+              client._initiateReconnects(_innerConnect, true);
+              client._processError('ERR_NEGOTIATE_TIMEOUT', `${client._serviceNameUCase}: ${client._serviceName} API negotiate timeout.`, undefined, logErrorsInner);
+            }
+          }));
+
+        } catch (error) {
+          socketClient.close();
+          socketClient = undefined;
+          client._initiateReconnects(_innerConnect, true);
+          return client._processError('ERR_CLIENT_CONNECT', `${client._serviceNameUCase}: Could not connect to ${client._serviceName} server ${client._baseURL}`, undefined, logErrorsInner);
+        }
+      }
+
+      // Okay, kick it off
+      _innerConnect(this, logErrors);
+    });
+  }
+
+
+  /****************************************************************************
+   **                                                                        **
+   ** Protected methods                                                      **
+   **                                                                        **
+   ****************************************************************************/
+
+   protected _timeoutCallback(callback: Function) : () => void
+   {
+     let called = false;
+
+     let timerID = setTimeout(() => {
+       if (called) return;
+         called = true;
+         callback(new PancakeError('ERR_CALLBACK_TIMEOUT'));
+     },
+     CONNECT_TIMEOUT*1000);
+
+     return function() {
+       if (called) return;
+       called = true;
+       clearTimeout(timerID);
+       callback.apply(this, arguments);
+     }
+   }
+
+
+  protected _processError(status: string, reason?: string, obj?: any, logError: boolean = true) : PancakeError
+  {
+    this._lastError = new PancakeError(status, reason, obj);
+    if (true === logError) {
+      log.trace(`${this._serviceNameUCase}: ${status}: ${reason}`);
+      if (obj) log.trace(obj);
+    }
+    return this._lastError;
+  }
+
+
+  /****************************************************************************
+   **                                                                        **
+   ** To override                                                            **
+   **                                                                        **
+   ****************************************************************************/
+
+  protected _performReconnectTasks() : void
+  {
+    // Do nothing here in the base class
+  }
+
+
+  protected _performConnectCleanup() : void
+  {
+    // Do nothing here in the base class
+  }
+
+
+  /****************************************************************************
+   **                                                                        **
+   ** Constructor                                                            **
+   **                                                                        **
+   ****************************************************************************/
+
+  constructor(serviceName: string, serverAPI: string, serverAPIVer: string)
+  {
+    super();
+
+    // Assignemnts
+    this._serviceName = serviceName;
+    this._serviceNameUCase = serviceName.toUpperCase();
+    this._serverAPI = serverAPI;
+    this._serverAPIVer = serverAPIVer;
+  }
+
+
+  /****************************************************************************
+   **                                                                        **
+   ** Public methods                                                         **
+   **                                                                        **
+   ****************************************************************************/
+
+  async connect(address: string, port: number, onConnect: ListenerCallback = undefined, onDisconnect: DisconnectCallback = undefined) : Promise<PancakeError>
+  {
+    // Close up shop and cancel any reconnect attempts
+    if (this._socket) {
+      this._socket.close();
+      this._socket = undefined;
+    }
+    this._connected = false;
+    this._cancelReconnects();
+
+    // Clear out active domains, channels, and subscriptions
+    this._performConnectCleanup();
+
+    // Clear out old event handlers
+    this.removeAllListeners();
+
+    // Remember these callbacks
+    if (onConnect) this.on('connect', onConnect);
+    if (onDisconnect) this.on('disconnect', onDisconnect);
+
+    // Build our URL
+    this._baseURL = URL_HTTP + address + ':' + port;
+
+    // Kick it all off
+    return this._connect(true);
+  }
+
+
+  get lastError() : PancakeError
+  {
+    return this._lastError;
+  }
+
+
+  get serviceName() : string
+  {
+    return this._serviceName;
+  }
+
+
+  get connected() : boolean
+  {
+    return this._connected;
+  }
+
+} // END class ClientAPI
